8e054dde4ae21710b67d3d0deb70a4c41ae85d58
matheusmarks <marquesmatheusoliveira01@gmail.com>
2022-01-29 23:34:25 -0300

    Working with bean's lifecycle

--- src/main/java/com/algaworks/algafood/api/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/api/service/AtivacaoClienteService.java
@@ -8,12 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class AtivacaoClienteService {
 
     @TipoDoNotificador(NivelUrgencia.URGENTE)
     @Autowired(required = false)
     private Notificador notificador;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("INIT" + notificador);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
+
 
     public void ativar(Client cliente) {
         cliente.ativar();
