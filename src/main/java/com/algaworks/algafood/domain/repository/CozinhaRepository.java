15d8ce2e15868616230b76fad89d7fe5ec11d8e7
matheusmarks <marquesmatheusoliveira01@gmail.com>
2025-05-28 22:29:30 -0300

    Implementing specifications with spring data jpa

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-    //List<Cozinha> listarPorNome(String nome);
-    //Testing if my gh user is correctgit status
+    List<Cozinha> findTodasByNomeContaining(String nome);
+
+    Optional<Cozinha> findByNome(String nome);
+
+    boolean existsByNome(String nome);
 
 }
--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -2,15 +2,18 @@ package com.algaworks.algafood.infrastructure.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
-import lombok.var;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -23,7 +26,33 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     public List<Restaurante> find(String nome,
                                   BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 
-        var jpql = new StringBuilder();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (StringUtils.hasText(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+        }
+
+        if (taxaFreteInicial != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+        }
+
+        if (taxaFreteFinal != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+        }
+
+        criteria.where(predicates.toArray(new Predicate[0]));
+        TypedQuery<Restaurante> query = entityManager.createQuery(criteria);
+
+        return query.getResultList();
+
+
+
+        /*var jpql = new StringBuilder();
         jpql.append("from Restaurante where 0 = 0 ");
 
         var parametros = new HashMap<String, Object>();
@@ -48,6 +77,6 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
         parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
 
-        return query.getResultList();
+        return query.getResultList();*/
     }
 }
