afdaf99a302d2e460e0a141390ec72486eb1a73e
matheusmarks <marquesmatheusoliveira01@gmail.com>
2025-06-20 16:04:10 -0300

    Adding flyway and migrations

---
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `cozinha` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
